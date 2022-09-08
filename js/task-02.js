79b0458f913c13bc73e8b50c76986c396267f4b2
lia2307 <juliatymchuk23@gmail.com>
2022-09-08 23:15:33 +0300

    done task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listItems = [];
+ingredients.forEach((element) => {
+  const items = document.createElement("li");
+  items.textContent = `${element}`;
+  items.classList.add("item");
+  return listItems.push(items);
+});
+console.log(listItems);
+
+const items = document.querySelector("ul#ingredients");
+items.append(...listItems);
