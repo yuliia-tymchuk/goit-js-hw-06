21cb7531b1bc8e41b0b391ae94c6c57170afc9cc
lia2307 <juliatymchuk23@gmail.com>
2022-09-14 22:57:04 +0300

    done task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,14 @@
-const counterValue = document.querySelector("#value");
-console.log(counterValue);
+let counterValue = 0;
+const refs = {
+  decrementBtn: document.querySelector('button[data-action="decrement"]'),
+  incrementBtn: document.querySelector('button[data-action="increment"]'),
+  valueEl: document.querySelector("#value"),
+};
+refs.decrementBtn.addEventListener("click", () => {
+  counterValue -= 1;
+  refs.valueEl.textContent = counterValue;
+});
+refs.incrementBtn.addEventListener("click", () => {
+  counterValue += 1;
+  refs.valueEl.textContent = counterValue;
+});
