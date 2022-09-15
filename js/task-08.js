bc2c3666ae6f68b368ae4d6949ef5b6b009dcaa9
lia2307 <juliatymchuk23@gmail.com>
2022-09-15 21:21:50 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const {
+    elements: { email, password },
+  } = e.currentTarget();
+  if (email.value === "" || password.value === "") {
+    return alert("Not all form fields are filled!");
+  }
+  e.currentTarget.reset();
+});
