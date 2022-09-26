a45d1686523d412e47c38f0ccda3ceebdd4b3152
lia2307 <juliatymchuk23@gmail.com>
2022-09-27 00:47:21 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -16,18 +16,28 @@ const images = [
 const listRef = document.querySelector(".gallery");
 console.log(listRef);
 
-const listItems = images.map(({ url, alt }) => {
-  const itemEl = document.createElement("li");
-  itemEl.classList.add("gallery_item");
-  const linkEl = document.createElement("a");
-  linkEl.href = "#";
-  const imgEl = document.createElement("img");
-  imgEl.src = url;
-  imgEl.alt = alt;
-  linkEl.append(imgEl);
-  itemEl.append(linkEl);
-  return itemEl;
-});
-listRef.append(...listItems);
+// const listItems = images.map(({ url, alt }) => {
+//   const itemEl = document.createElement("li");
+//   itemEl.classList.add("gallery_item");
+//   const linkEl = document.createElement("a");
+//   linkEl.href = "#";
+//   const imgEl = document.createElement("img");
+//   imgEl.src = url;
+//   imgEl.alt = alt;
+//   linkEl.append(imgEl);
+//   itemEl.append(linkEl);
+//   return itemEl;
+// });
+// listRef.append(...listItems);
 
-console.log(listRef.innerHTML);
+// console.log(listRef.innerHTML);
+
+const listItems = images.map(({ url, alt }) => {
+  return ` <li  class="gallery_item"><a href="#"><img src="${url}" alt="${alt}"></a></li>`;
+});
+console.log(listItems);
+
+const listEl = listItems.join("");
+console.log(listEl);
+
+listRef.insertAdjacentHTML("afterbegin", listItems);
